5381e9ee97e43e967b9b88a768d20f5d42c9aaa2
tugceyalcin <50701569+tugceyalcin@users.noreply.github.com>
2024-05-29 21:25:43 +0300

    Personnel nesnesi modellendi. Personnel nesnesi xml formatında oluşturuldu. Personnel nesnesi yaml formatında oluşturuldu. Personnel nesnesi json formatında oluşturuldu.

---
+++ src/main/java/personnel.java
@@ -0,0 +1,23 @@
+public class personnel {
+    private String name;
+    private int age;
+    private String position;
+    private String department;
+    private String project;
+    private int salary;
+
+    private int experience;
+
+
+    public personnel(String name, int age, String position, String department, String project, int salary, int experience) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.department = department;
+        this.salary = salary;
+        this.project= project;
+        this.experience= experience;
+    }
+
+
+}
